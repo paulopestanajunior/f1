66f51c15679559cf300cb044a1d78fb0fda0f05b
paulo.pestana <paulo.pestana@g.globo>
2026-01-15 16:59:36 -0300

    alterando cors

--- backend/main.py
+++ backend/main.py
@@ -77,7 +77,10 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:8080",
         "http://127.0.0.1:8080",
+        "https://f1-three-chi.vercel.app",
+        
     ],
+    allow_origin_regex=r"^https://.*\.vercel\.app$",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
